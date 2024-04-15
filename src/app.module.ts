3156e8574dc8c30c91349cc11ff77ad2f0b2101f
Neliia Zelienova <neliia@vormats.com>
2024-04-15 14:40:41 +0200

    Added socket io connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,20 @@ import { RoomsController } from './controllers/rooms/rooms.controller';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { PrismaService } from './services/prisma/prisma.service';
 import { UsersService } from './services/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { SocketGateway } from './gateway/gateway';
 
 @Module({
-  imports: [],
+  imports: [SocketGateway],
   controllers: [RoomsController],
-  providers: [PrismaService, UsersService, {
-    provide: APP_GUARD,
-    useClass: AuthGuard,
-  },],
+  providers: [
+    PrismaService,
+    UsersService,
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
